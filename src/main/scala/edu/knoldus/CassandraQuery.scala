eb1e4a6c5a5045736355ba5aff981cb958f6bf27
Ayush-Singhal28 <ayush.singhal@knoldus.in>
2018-03-16 10:57:18 +0530

    operation module added

--- src/main/scala/edu/knoldus/CassandraQuery.scala
+++ src/main/scala/edu/knoldus/CassandraQuery.scala
@@ -2,10 +2,11 @@ package edu.knoldus
 
 import com.datastax.driver.core.{ConsistencyLevel, Session}
 
-import scala.collection.JavaConverters._
+
 
 object CassandraQuery extends App with CassandraProvider {
 
+  val operation = new Operation
   cassandraSession.getCluster.getConfiguration.getQueryOptions.setConsistencyLevel(ConsistencyLevel.QUORUM)
 
 
@@ -15,63 +16,14 @@ object CassandraQuery extends App with CassandraProvider {
   val empSalary = 30000
   val empCity = "Chandigarh"
 
-  creatingEmployeeTable(cassandraSession)
-  insertingDataToTable(cassandraSession)
-  fetchingDatabyId(cassandraSession, empId)
-  updatingData(cassandraSession, empId1, empSalary1)
-  gettingDataBySalary(cassandraSession, empSalary, empId1)
-  gettingDataByCity(cassandraSession, empCity)
-  deletingDataByCity(cassandraSession, empCity)
+  operation.creatingEmployeeTable(cassandraSession)
+  operation.insertingDataToTable(cassandraSession)
+  operation.fetchingDatabyId(cassandraSession, empId)
+  operation.updatingData(cassandraSession, empId1, empSalary1)
+  operation.gettingDataBySalary(cassandraSession, empSalary, empId1)
+  operation.gettingDataByCity(cassandraSession, empCity)
+  operation.deletingDataByCity(cassandraSession, empCity)
 
-  def creatingEmployeeTable(cassandraSession: Session): Unit = {
-    cassandraSession.execute("create table if not exists employee(emp_id int ,emp_name text,emp_city text,emp_salary, varint,emp_phone varint, primary key(emp_id,emp_salary))")
-  }
-
-  def insertingDataToTable(cassandraSession: Session): Unit = {
-
-    cassandraSession.execute("insert into employee(emp_id, emp_name, emp_city, emp_salary, emp_phone) VALUES(1, 'Ayush', 'Delhi', 50000, 9876543210)")
-    cassandraSession.execute("insert into employee(emp_id, emp_name, emp_city, emp_salary, emp_phone) values( 2,'Vaibhav','Chandigarh',30000,9897888654)")
-    cassandraSession.execute("insert into employee(emp_id, emp_name, emp_city, emp_salary, emp_phone) values(3,'Sagar','Banaras',70000,8450003459")
-    cassandraSession.execute("insert into employee(emp_id, emp_name, emp_city, emp_salary, emp_phone) values(4,'Vatan','Chandigarh',20000,8278979990")
-  }
-
-
-  def fetchingDatabyId(cassandraSession: Session, emp_id: Int): Unit = {
-
-    val result = cassandraSession.execute(s"select * from employee where emp_id=${emp_id}").asScala.toList
-    result.foreach(println(_))
-
-  }
-
-  def updatingData(cassandraSession: Session, id: Int, salary: Int): Unit = {
-    cassandraSession.execute(s"update employee set city='chandigarh' where emp_id=${id} AND salary=${salary}")
-
-  }
-
-  def gettingDataBySalary(cassandraSession: Session, salary: Int, id: Int): Unit = {
-    val result = cassandraSession.execute(s"select * from employee where emp_id =${id} AND emp_salary > ${salary}")
-    result.forEach(println(_))
-
-  }
-
-  def gettingDataByCity(cassandraSession: Session, city: String): Unit = {
-
-
-    val result = cassandraSession.execute(s"select * from employee where emp_city ='${city}'").asScala.toList
-    result.foreach(println(_))
-
-  }
-
-  private def deletingDataByCity(cassandraSession: Session, city: String): Unit = {
-    cassandraSession.execute("create table newEmployee(emp_id int,emp_name text,emp_city text,emp_salary varint,emp_phone varint,primary key(emp_city));")
-    cassandraSession.execute("insert into newEmployee(emp_id, emp_name, emp_city, emp_salary, emp_phone) VALUES(1, 'Ayush', 'Delhi', 50000, 9876543210)")
-    cassandraSession.execute("insert into newEmployee(emp_id, emp_name, emp_city, emp_salary, emp_phone) values( 2,'Vaibhav','Chandigarh',30000,9897888654)")
-    cassandraSession.execute("insert into newEmployee(emp_id, emp_name, emp_city, emp_salary, emp_phone) values(3,'Sagar','Banaras',70000,8450003459")
-    cassandraSession.execute("insert into newEmployee(emp_id, emp_name, emp_city, emp_salary, emp_phone) values(4,'Vatan','Chandigarh',20000,8278979990")
-    cassandraSession.execute(s"delete from newEmployee where emp_city = '${city}'")
-    val result = cassandraSession.execute("select * from newEmployee").asScala.toList
-    result.foreach(println(_))
-  }
 
 
 }
